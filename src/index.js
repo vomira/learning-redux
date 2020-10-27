22e25e591ab18ed68143028f55d2cfda84146cdf
Mira Vogt <miravogt@gmail.com>
2020-10-27 20:30:14 +0100

    build first feature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import index from './js/index';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./js/store/index";
+import App from "./js/components/App";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+  
+) ;
