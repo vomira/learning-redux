3339ca7d53b6d6ad71a399ede386ed09e8786b53
Mira Vogt <miravogt@gmail.com>
2020-10-30 10:44:03 +0100

    add redux thunk, async mw

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -2,6 +2,7 @@
 import React from "react";
 import List from "./List";
 import Form from "./Form";
+import Post from "./Posts";
 
 const App = () => {
 
@@ -14,6 +15,10 @@ return(
     <div>
       <h2>Add a new article</h2>
       <Form />
+    </div>
+    <div>
+        <h2>API posts</h2>
+        <Post />
     </div>
   </>
 )};
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,11 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers/index';
 import { forbiddenWordsMiddleware } from "../middleware/index";
+import thunk from "redux-thunk";
+
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 //reducers produce the state of our application
 const store = createStore(
     rootReducer,
-    applyMiddleware(forbiddenWordsMiddleware)
+    storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk))
     );
 
 export default store;
